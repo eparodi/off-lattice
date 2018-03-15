dcd48fe36a27ffbc0aeabd4d9987c67ed97c449d
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2018-03-14 21:48:41 -0300

    Fix Va calculator program

--- variables.py
+++ variables.py
@@ -2,7 +2,7 @@ import numpy as np
 
 SPEED = 0.03
 
-with open('outp5.data') as f:
+with open('output.data') as f:
     numberOfParticles = None
     particle = 0
     velocity = [0,0]
@@ -14,17 +14,17 @@ with open('outp5.data') as f:
         if (time == None):
             time = int(line)
             continue
-        numbers = line.strip()
+        numbers = line.split()
         angle = float(numbers[-1])
-        velocity[0] += np.cos(angle) * SPEED
-        velocity[1] += np.sin(angle) * SPEED
+        velocity[0] += np.cos(angle)
+        velocity[1] += np.sin(angle)
         particle += 1
         if (particle == numberOfParticles):
             particle = 0
             module = np.sqrt(velocity[0] ** 2 + velocity[1] ** 2)
             print("{} {}".format(
                     time,
-                    module /(numberOfParticles * SPEED),
+                    module /(numberOfParticles),
                 )
             )
             velocity = [0,0]
